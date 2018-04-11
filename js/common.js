995ac61fd9d0cb12fe3dc5c18ace8d524a9815e8
lan_kruzer <ar-48@yandex,ru>
2018-04-11 15:13:06 +0300

    complete header and index-screen-1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 $(function() {
 
-	// Custom JS
-
+	$('.toggle-mobile-menu').click(function() {
+		$('body').toggleClass('hide-scroll');
+		$(this).toggleClass('toggle-mobile-menu_active');
+		$('.header-nav-list').toggleClass('header-nav-list_active');
+	})
 });
