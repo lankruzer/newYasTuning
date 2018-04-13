c4c536b6ec0d42490624f91f9a0bf78fe0b18171
lan_kruzer <ar-48@yandex,ru>
2018-04-13 09:55:36 +0300

    add index-screen-3

--- js/common.js
+++ js/common.js
@@ -1,8 +1,35 @@
 $(function() {
 
+	// mobile menu
 	$('.toggle-mobile-menu').click(function() {
 		$('body').toggleClass('hide-scroll');
 		$(this).toggleClass('toggle-mobile-menu_active');
 		$('.header-nav-list').toggleClass('header-nav-list_active');
 	})
+	// /mobile menu
+
+	// index slider
+	$('.slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	})
+	// /index slider
+
+
 });
